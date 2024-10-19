c7c1565b9bd152fc822c2f70a44abb022e413885
Zulfikar <zjuniarto@gmail.com>
2024-10-19 21:33:41 +0700

    update deteksi wajah v1

--- main.py
+++ main.py
@@ -12,11 +12,24 @@ if not kamera:
 while True:
     
     ret, frame = kamera.read()
+    kamera.set(cv.CAP_PROP_FPS,30)
+    
 
     if ret == False:
         print('KEsalahan System') 
+    
+    try :
+        addcolor = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                
+        filefaceDetect = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_alt.xml") 
+        faceDetector =  filefaceDetect.detectMultiScale(addcolor, scaleFactor=1.1, minNeighbors=1, minSize=((20,20)))
         
-    addcolor = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+     
+        for (x, y, w, h) in faceDetector:
+            cv.rectangle(addcolor, (x, y), (x + w, y + h), (255, 255, 0), 2)
+            
+    except:
+        print('Tidak file seperti itu')
     
     cv.imshow("camera",addcolor)
     
---
+++ readme.md
@@ -0,0 +1,2 @@
+#   Program Pendeteksi Wajah Pada manusia
+#   menggunakan opencv dan Imtuils
